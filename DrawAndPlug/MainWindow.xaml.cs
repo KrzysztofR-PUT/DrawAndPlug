0f886a04bafd9394096c3b8fcfbca88bddbf8308
KrzysztofR-PUT <krzysztof.rozga@student.put.poznan.pl>
2017-06-03 15:18:08 +0200

    Add project files.

---
+++ DrawAndPlug/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DrawAndPlug
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Save_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Undo_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Redo_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddPlugin_Clicked(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
