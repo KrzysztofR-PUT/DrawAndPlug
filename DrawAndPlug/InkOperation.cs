07747d8393cd7bbf3a531fb804bbd919828e9673
KrzysztofR-PUT <krzysztof.rozga@student.put.poznan.pl>
2017-06-04 00:45:45 +0200

    Added ink colors, image save operation, undo/redo (only 1 op.  - otherwise crashes).

---
+++ DrawAndPlug/InkOperation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Ink;
+using System.Windows.Controls;
+
+
+
+namespace DrawAndPlug
+{
+    class InkOperation
+    {
+        public static BitmapFrame CreateBitmapSource(InkCanvas inkCanvas)
+        {
+            int margin = (int)inkCanvas.Margin.Left;
+            int width = (int)inkCanvas.ActualWidth - margin;
+            int height = (int)inkCanvas.ActualHeight - margin;
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap(width, height, 96d, 96d, PixelFormats.Default);
+            rtb.Render(inkCanvas);
+
+            BitmapFrame frame = BitmapFrame.Create(rtb);
+
+            return frame;
+        }
+
+
+        public static BitmapFrame CreateBitmapFromInk(StrokeCollection ink, BitmapSource background)
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                drawingContext.DrawImage(background, new Rect(0, 0, background.Width, background.Height));
+
+                foreach (var item in ink)
+                {
+                    item.Draw(drawingContext);
+                }
+                drawingContext.Close();
+                var bitmap = new RenderTargetBitmap((int)background.Width, (int)background.Height, background.DpiX, background.DpiY, PixelFormats.Pbgra32);
+                bitmap.Render(drawingVisual);
+                return BitmapFrame.Create(bitmap);
+            }
+        }
+    }
+}
--- DrawAndPlug/MainWindow.xaml.cs
+++ DrawAndPlug/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace DrawAndPlug
 {
@@ -20,29 +22,86 @@ namespace DrawAndPlug
     /// </summary>
     public partial class MainWindow : Window
     {
+        System.Windows.Ink.StrokeCollection _added;
+        System.Windows.Ink.StrokeCollection _removed;
+        private bool handle = true;
+
         public MainWindow()
         {
             InitializeComponent();
+            inkCanvas.Strokes.StrokesChanged += Strokes_StrokesChanged;
+        }
+
+        private void Strokes_StrokesChanged(object sender, System.Windows.Ink.StrokeCollectionChangedEventArgs e)
+        {
+            if (handle)
+            {
+                _added = e.Added;
+                _removed = e.Removed;
+            }
         }
 
         private void Save_Clicked(object sender, RoutedEventArgs e)
         {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "PNG file (*.png)|*.png";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                BitmapFrame frame = InkOperation.CreateBitmapSource(inkCanvas);
 
+                FileStream stream = new FileStream(saveFileDialog.FileName, FileMode.Create);
+                var encoder = new PngBitmapEncoder();
+                encoder.Interlace = PngInterlaceOption.On;
+                encoder.Frames.Add(frame);
+                encoder.Save(stream);
+                stream.Close();
+            }
         }
 
         private void Undo_Clicked(object sender, RoutedEventArgs e)
         {
-
+            handle = false;
+            inkCanvas.Strokes.Remove(_added);
+            inkCanvas.Strokes.Add(_removed);
+            handle = true;
         }
 
         private void Redo_Clicked(object sender, RoutedEventArgs e)
         {
-
+            handle = false;
+            inkCanvas.Strokes.Add(_added);
+            inkCanvas.Strokes.Remove(_removed);
+            handle = true;
         }
 
         private void AddPlugin_Clicked(object sender, RoutedEventArgs e)
         {
+            
+        }
 
+        private void GreenStroke_Clicked(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Green;
+        }
+
+        private void BlackStroke_Clicked(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Black;
+        }
+
+        private void BlueStroke_Clicked(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Blue;
+        }
+
+        private void RedStroke_Clicked(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.DefaultDrawingAttributes.Color = Colors.Red;
+        }
+
+        private void Clear_Clicked(object sender, RoutedEventArgs e)
+        {
+            inkCanvas.Strokes.Clear();
         }
     }
 }
